96068129879af618d8822816bbecbc946effa27b
a8anassis <a8anassis@gmail.com>
2025-10-15 22:18:45 +0300

    feat: add endpoints for signup and login

--- SchoolApp/Controllers/UsersController.cs
+++ SchoolApp/Controllers/UsersController.cs
@@ -16,21 +16,48 @@ namespace SchoolApp.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<UserReadOnlyDTO> SignupUserTeacherAsync(TeacherSignupDTO teacherSignupDTO)
+        public async Task<ActionResult<UserReadOnlyDTO>> SignupUserTeacherAsync(TeacherSignupDTO teacherSignupDTO)
         {
-           if (!ModelState.IsValid)
-           {
+            if (!ModelState.IsValid)
+            {
                var errors = ModelState.Where(e => e.Value!.Errors.Any())
                    .Select(e => new { 
                        Field = e.Key, 
                        Errors = e.Value!.Errors.Select(er => er.ErrorMessage).ToArray()
                    });
                 throw new InvalidRegistrationException("ErrorsInRegistration" + errors);
-           }
+            }
 
-           UserReadOnlyDTO returnedUserDTO = await applicationService.TeacherService.SignupUserAsync(teacherSignupDTO);
+            UserReadOnlyDTO returnedUserDTO = await applicationService.TeacherService.SignUpUserAsync(teacherSignupDTO);
+            return CreatedAtAction(nameof(GetUserById), new { id = returnedUserDTO.Id }, returnedUserDTO);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserReadOnlyDTO>> GetUserById(int id)
+        {
+            UserReadOnlyDTO userReadOnlyDTO = await applicationService.UserService.GetUserByIdAsync(id) 
+                ?? throw new EntityNotFoundException("User", "User with id " + id + " not found");
+           
+            return Ok(userReadOnlyDTO);
+        }
 
+        [HttpPost]
+        public async Task<ActionResult<JwtTokenDTO>> LoginUserAsync(UserLoginDTO credentials)
+        {
+            var user = await applicationService.UserService.VerifyAndGetUserAsync(credentials) 
+                ?? throw new EntityNotAuthorizedException("User", "Bad Credentials");
+
+            var token = applicationService.UserService.CreateUserToken(user.Id, user.Username, user.Email, 
+                user.UserRole, configuration["Authentication:SecretKey"]!);
+            JwtTokenDTO userToken = new JwtTokenDTO { Token = token };
+            return Ok(userToken);
+        }
+
+        [HttpGet("{username}")]
+        public async Task<ActionResult<UserTeacherReadOnlyDTO>> GetUserTeacherByUsernameAsync(string? username)
+        {
+            var returnedUserDTO = await applicationService.UserService.GetUserTeacherByUsernameAsync(username!);
+            return Ok(returnedUserDTO);
+        }
     }
 }
